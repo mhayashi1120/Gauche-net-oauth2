a2961c4b1a3cccaf9b4f79f4a5aa5f0dc8b508bd
Masahiro Hayashi <mhayashi1120@gmail.com>
2020-01-14 19:37:36 +0900

    update copyright import new `gauche-package generate`

---
+++ package.scm
@@ -0,0 +1,45 @@
+;;
+;; Package Gauche-net-oauth2
+;;
+
+(define-gauche-package "Gauche-net-oauth2"
+  ;; 
+  :version "0.1.1"
+
+  ;; Description of the package.  The first line is used as a short
+  ;; summary.
+  :description "OAuth2 client library.\n\
+                "
+
+  ;; List of dependencies.
+  ;; Example:
+  ;;     :require (("Gauche" (>= "0.9.5"))  ; requires Gauche 0.9.5 or later
+  ;;               ("Gauche-gl" "0.6"))     ; and Gauche-gl 0.6
+  :require ()
+
+  ;; List of providing modules
+  ;; NB: This will be recognized >= Gauche 0.9.7.
+  ;; Example:
+  ;;      :providing-modules (util.algorithm1 util.algorithm1.option)
+  :providing-modules ()
+  
+  ;; List name and contact info of authors.
+  ;; e.g. ("Eva Lu Ator <eval@example.com>"
+  ;;       "Alyssa P. Hacker <lisper@example.com>")
+  :authors ("Masahiro Hayashi <mhayashi1120@gmail.com>")
+
+  ;; List name and contact info of package maintainers, if they differ
+  ;; from authors.
+  ;; e.g. ("Cy D. Fect <c@example.com>")
+  :maintainers ()
+
+  ;; List licenses
+  ;; e.g. ("BSD")
+  :licenses ("BSD")
+
+  ;; Homepage URL, if any.
+  ; :homepage "http://example.com/Gauche-net-oauth2/"
+
+  ;; Repository URL, e.g. github
+  ; :repository "http://example.com/Gauche-net-oauth2.git"
+  )
--- test.scm
+++ test.scm
@@ -8,8 +8,13 @@
 (use net.oauth2)
 (test-module 'net.oauth2)
 
+;; The following is a dummy test code.
+;; Replace it for your tests.
+;; (test* "test-net_oauth2" "net_oauth2 is working"
+;;        (test-net_oauth2))
+
 ;; If you don't want `gosh' to exit with nonzero status even if
-;; test fails, pass #f to :exit-on-failure.
+;; the test fails, pass #f to :exit-on-failure.
 (test-end :exit-on-failure #t)
 
 
